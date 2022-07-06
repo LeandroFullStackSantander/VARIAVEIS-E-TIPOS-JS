1388a6c9e493cec352b8e0f780083b6216a74a1a
Leandro Castro <106716899+LeandroFullStackSantander@users.noreply.github.com>
2022-07-06 16:38:36 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+const FIRST_NAME = "LEANDRO CASTRO";
+const FIRST_NAME_1 = "MACIEL";
+console.log(FIRST_NAME, FIRST_NAME_1);  
---
+++ tipos.js
@@ -0,0 +1,26 @@
+/* STRINGS */
+/*COMUMENTE UTILIZADA PARA TEXTOS
+  VALORES DECLARADOS ENTRE ASPAS OU CRASES 
+  
+  metodos importantes
+  
+  concatenação
+  propriedade length
+  iterabilidiade
+  formatação
+  
+  NUMEROS
+  ARRAYS
+  lista iteravel de elementos
+  OBJETOS
+  estrutura do tipo chave{} -> valor:"xx"
+    [empty = 0, "", [], {}, 
+    null = não exista; nulo;
+    e undefined = indefinido, não existe!
+    = são variaveis que lhe 
+    retornaram valores falsos
+  
+  
+  
+  */
+  
